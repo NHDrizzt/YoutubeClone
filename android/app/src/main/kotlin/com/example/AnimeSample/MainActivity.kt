17bc9cc47e315e1cf98f972d92ae2aad9cb3bccc
Rokol <biel_s.carvalho2@hotmail.com>
2020-10-24 19:07:47 -0300

    first

---
+++ android/app/src/main/kotlin/com/example/AnimeSample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.AnimeSample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Anime Info',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'My Anime Info'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.red,
+      home: DefaultTabController(
+        length: 5,
+        child: new Scaffold(
+          body: TabBarView(
+            children: [
+              new Container(
+                color: Colors.red,
+              ),
+              new Container(
+                color: Colors.orange,
+              ),
+              new Container(
+                color: Colors.lightGreen,
+              ),
+              new Container(
+                color: Colors.red,
+              ),
+              new Container(
+                color: Colors.blue,
+              ),
+            ],
+          ),
+          bottomNavigationBar: new TabBar(
+            tabs: [
+              Tab(
+                icon: new Icon(Icons.home),
+              ),
+              Tab(
+                icon: new Icon(Icons.info),
+              ),
+              Tab(
+                icon: new Icon(Icons.perm_identity),
+              ),
+              Tab(
+                icon: new Icon(Icons.add_circle),
+              ),
+              Tab(
+                icon: new Icon(Icons.settings),
+              )
+            ],
+            labelColor: Colors.brown,
+            unselectedLabelColor: Colors.blue,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorColor: Colors.red,
+          ),
+          backgroundColor: Colors.black,
+        ),
+      ),
+    );
+  }
+}
